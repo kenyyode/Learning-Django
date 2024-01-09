390b4896a199ce5f5fae667d3a0cc1610391eb55
kenyyode <samsonsalvation@gmail.com>
2024-01-09 21:40:43 +0100

    Update

--- playground/views.py
+++ playground/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def say_hello (request):
     return HttpResponse("Hello!")
+
+def index(request):
+    return HttpResponse("Hello, World. This is the index of Demoapp")
